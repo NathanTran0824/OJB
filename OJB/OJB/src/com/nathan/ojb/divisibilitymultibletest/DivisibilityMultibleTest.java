8ee009bca3669bff1404dcacecb2a21141e95460
NathanTran0824 <nathan.tran0824@gmail.com>
2015-09-11 17:44:59 +0700

    Format code

--- OJB/OJB/src/com/nathan/ojb/divisibilitymultibletest/DivisibilityMultibleTest.java
+++ OJB/OJB/src/com/nathan/ojb/divisibilitymultibletest/DivisibilityMultibleTest.java
@@ -10,10 +10,10 @@ public class DivisibilityMultibleTest {
 	public static void main(String[] args) {
 		int numberOne = 24;
 		int numberTwo = 8;
-		
+
 		if ((numberOne % numberTwo) == 0) {
 			System.out.println(numberOne + " is a multiple of " + numberTwo);
-		}else {
+		} else {
 			System.out.println(numberOne + " is not a multiple of " + numberTwo);
 		}
 	}
