90968c1a2997fe9f6de106afdbc786e42c17c20d
NathanTran0824 <nathan.tran0824@gmail.com>
2015-09-11 17:47:47 +0700

    Rename

--- OJB/OJB/src/com/nathan/ojb/divisibilitymultibletest/DivisibilityMultipleTest.java
+++ OJB/OJB/src/com/nathan/ojb/divisibilitymultibletest/DivisibilityMultipleTest.java
@@ -5,7 +5,7 @@ package com.nathan.ojb.divisibilitymultibletest;
  * 
  * @author Nathan
  */
-public class DivisibilityMultibleTest {
+public class DivisibilityMultipleTest {
 
 	public static void main(String[] args) {
 		int numberOne = 24;
