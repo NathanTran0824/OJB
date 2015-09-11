f02fe93693dcf0934d6575d25ea6686865d93103
NathanTran0824 <nathan.tran0824@gmail.com>
2015-09-11 17:02:09 +0700

    Create CharacterTypeCastingInteger

---
+++ OJB/OJB/src/com/nathan/ojb/charactertypecastinginteger/CharacterTypeCastingInteger.java
@@ -0,0 +1,16 @@
+package com.nathan.ojb.charactertypecastinginteger;
+
+/**
+ * CharacterTypeCastingInteger: Tue,20150908
+ * 
+ * @author Nathan
+ */
+public class CharacterTypeCastingInteger {
+
+	public static void main(String[] args) {
+		char character = 'A';
+		int typeCastingInteger = character;
+		System.out.println(character + " = " + typeCastingInteger);
+	}
+
+}
