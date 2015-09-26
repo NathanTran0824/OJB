48d1d8da47435314968811671caff1e988e09570
NathanTran0824 <nathan.tran0824@gmail.com>
2015-09-26 23:40:16 +0700

    Create CharacterClass

---
+++ OJB/OJB/src/com/nathan/ojb/characterclass/CharacterClass.java
@@ -0,0 +1,15 @@
+package com.nathan.ojb.characterclass;
+
+public class CharacterClass {
+
+	public static void main(String[] args) {
+		char isDigit = '1';
+		char noDigit = 'a';
+		char isWhitespace = '1';
+		char noWhitespace = '1';
+		System.out.println(Character.isDigit(isDigit) + " - " + Character.isDigit(noDigit));
+		System.out.println(Character.isWhitespace(isWhitespace) + " - " + Character.isWhitespace(noDigit));
+
+	}
+
+}
