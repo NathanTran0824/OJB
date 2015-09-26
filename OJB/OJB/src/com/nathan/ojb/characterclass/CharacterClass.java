bfb1eb7b6d17e16f00d7f5451860626817ae5c5b
NathanTran0824 <nathan.tran0824@gmail.com>
2015-09-27 00:06:05 +0700

    Modifiy

--- OJB/OJB/src/com/nathan/ojb/characterclass/CharacterClass.java
+++ OJB/OJB/src/com/nathan/ojb/characterclass/CharacterClass.java
@@ -5,10 +5,10 @@ public class CharacterClass {
 	public static void main(String[] args) {
 		char isDigit = '1';
 		char noDigit = 'a';
-		char isWhitespace = '1';
+		char isWhitespace = ' ';
 		char noWhitespace = '1';
 		System.out.println(Character.isDigit(isDigit) + " - " + Character.isDigit(noDigit));
-		System.out.println(Character.isWhitespace(isWhitespace) + " - " + Character.isWhitespace(noDigit));
+		System.out.println(Character.isWhitespace(isWhitespace) + " - " + Character.isWhitespace(noWhitespace));
 
 	}
 
